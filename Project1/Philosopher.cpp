39de7eb0b393f07b739c9bc762b9cc7e99f2ab41
Jemdrek03 <j.radlowski03@gmail.com>
2025-04-07 03:53:13 +0200

    chore: changed 1 (one) more comment to english

--- Project1/Philosopher.cpp
+++ Project1/Philosopher.cpp
@@ -97,7 +97,7 @@ void eating(int id, int forkies[], int howMany){
     std::cout << "Philosopher " << id << " is currently eating\n";
     for(int i = 0; i < howMany; i++)
     {
-        std::cout<<"Widelec "<<i<<" trzymany przez: "<<forkies[i]<<std::endl;
+        std::cout<<"Fork "<<i<<" is being held by: "<<forkies[i]<<std::endl;
     }
     output.unlock();
     std::this_thread::sleep_for(std::chrono::milliseconds (500));
